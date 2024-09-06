9cbef29783a0e1d73ec0ac255a86ac69841e214b
JJoriping <op@jjo.kr>
2024-09-07 00:40:33 +0900

    Add standard subscriber to the logger instance

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,8 @@
 import { col, Logger } from "@daldalso/logger";
+import createStandardSubscriber from "@daldalso/logger/subscribers/standard.js";
 
 export const logger = new Logger({
   headerFormat: col.lMagenta`tailwind-base` + " $H │ ",
   indent: 18
 });
+logger.addSubscriber(createStandardSubscriber(), { colored: true });
